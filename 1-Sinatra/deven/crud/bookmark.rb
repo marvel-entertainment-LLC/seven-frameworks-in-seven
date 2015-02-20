97c2206e4581c6dabef06a92bbefe6fdce4a89aa
Deven Xie <dxie@marvel.com>
2015-02-19 22:46:43 -0500

    Add creation timestamp to bookmark resource.

--- 1-Sinatra/deven/crud/bookmark.rb
+++ 1-Sinatra/deven/crud/bookmark.rb
@@ -1,8 +1,11 @@
 require "data_mapper"
+require "dm-timestamps"
+
 class Bookmark
   include DataMapper::Resource
 
   property :id, Serial
   property :url, String
   property :title, String
+  property :created_at, DateTime
 end
