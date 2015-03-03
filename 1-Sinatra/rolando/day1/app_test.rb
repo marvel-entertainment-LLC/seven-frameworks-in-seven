9f6c966beb254a53676e1254cdb084c778bd94a8
roshow <rolando@roshow.net>
2015-03-02 23:36:36 -0500

    test for delete

--- 1-Sinatra/rolando/day1/app_test.rb
+++ 1-Sinatra/rolando/day1/app_test.rb
@@ -38,7 +38,7 @@ describe "Bookmarking App" do
     post "/bookmarks",
       {:url => "http://www.test.com", :title => "Test"}
     bookmark_uri = last_response.body
-    id = bookmark_uri.split("/").last # (4)
+    id = bookmark_uri.split("/").last 
     
     put "/bookmarks/#{id}", {:title => "Success"} 
     last_response.status.should == 204
@@ -46,6 +46,24 @@ describe "Bookmarking App" do
     get "/bookmarks/#{id}"
     retrieved_bookmark = JSON.parse(last_response.body)
     expect(retrieved_bookmark["title"]).to eq("Success") 
+  end
+
+  it "deletes a bookmark" do
+    post "/bookmarks",
+      {:url => "http://www.test.com", :title => "Test"}
+    bookmark_uri = last_response.body
+    id = bookmark_uri.split("/").last 
+    get "/bookmarks"
+    bookmarks = JSON.parse(last_response.body)
+    last_size = bookmarks.size 
+
+    delete "/bookmarks/#{id}"
+    last_response.status.should == 200
+
+    get "/bookmarks"
+    bookmarks = JSON.parse(last_response.body)
+    expect(bookmarks.size).to eq(last_size - 1) 
+
   end
 
 end
